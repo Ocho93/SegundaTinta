9ec8d83bbcb113f5639ba4c6332006bff8061907
Ocho93 <ar.sanchez.p93@gmail.com>
2024-02-22 00:09:57 +0100

    estilos navbar

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
+import { useState } from "react";
 // import './App.css'
-import Header from './components/header/header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
+import Header from "./components/header/header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
     </div>
   );
 }
 
-export default App
+export default App;
--- FRONT/src/components/header/header.jsx
+++ FRONT/src/components/header/header.jsx
@@ -1,27 +1,61 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "./header.css";
 
 function NavScrollExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="custom-navbar">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand href="#">
+          <img
+            src="../../../Logo.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Logo"
+          />
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          />
+          <Nav
+            className="me-auto my-2 my-lg-0 text-center"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          />
+          <Nav
+            className="me-auto my-2 my-lg-0 text-center"
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <Nav
+              className="me-auto my-2 my-lg-0 text-center"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1" className="me-6 link-item">
+                Galería
+              </Nav.Link>
+              <Nav.Link href="#action2" className="me-6 link-item">
+                Contacto
+              </Nav.Link>
+              <Nav.Link href="#action3" className="link-item">
+                Venta
+              </Nav.Link>
+            </Nav>
+
+            {/* <NavDropdown title="Idioma" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Inglés</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
                 Another action
               </NavDropdown.Item>
@@ -32,7 +66,7 @@ function NavScrollExample() {
             </NavDropdown>
             <Nav.Link href="#" disabled>
               Link
-            </Nav.Link>
+            </Nav.Link> */}
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -41,7 +75,13 @@ function NavScrollExample() {
               className="me-2"
               aria-label="Search"
             />
-            <Button variant="outline-success">Search</Button>
+            <Button
+              variant="outline-success"
+              className="custom-search-button custom-search-button-header"
+            >
+              Search
+            </Button>{" "}
+            {/* Agrega la clase custom-search-button-header */}
           </Form>
         </Navbar.Collapse>
       </Container>
